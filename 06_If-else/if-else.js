1701d0cc2fb0c3a2f0f3ff9b2dcd42c75ed4a66b
shivam24s <shivamshrivastav8055@gmail.com>
2026-02-02 17:12:28 +0530

    type coersion and if else code added

---
+++ 06_If-else/if-else.js
@@ -0,0 +1,15 @@
+let age = 18;
+
+if (age >= 18) {
+  console.log("you can give vote");
+}
+
+// if -else
+
+let drivingLicense = false;
+
+if (drivingLicense) {
+  console.log("you can drive vehicle");
+} else {
+  console.log("you can't drive vehicle");
+}
---
+++ 06_If-else/ladder.js
@@ -0,0 +1,15 @@
+// multiple condition
+
+let marks = 80;
+
+if (marks >= 90) {
+  console.log("you have achieved A grade");
+} else if (marks >= 70) {
+  console.log("you have achieved B grade");
+} else if (marks >= 50) {
+  console.log("you have achieved c grade");
+} else if (marks >= 35) {
+  console.log("you have achieved D grade");
+} else {
+  console.log("you have failed this examination");
+}
