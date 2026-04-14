1c0be728a91943ac1a200bab004c41cafe36a049
shivam24s <shivamshrivastav8055@gmail.com>
2026-04-14 16:39:04 +0530

    typescript code added

---
+++ 23_typescript/app.ts
@@ -0,0 +1,17 @@
+let userName: string = "alice";
+
+let num: any = "24";
+
+let num1: number = 10;
+let num2: number = 20;
+
+function add(a:number,b:number):number {
+
+    return a+b;
+
+
+}
+
+
+console.log(add(num1,num2))
+
